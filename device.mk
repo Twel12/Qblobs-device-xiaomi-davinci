4fbceed6b844a73dbb82d76d840da781c2fa45a1
daniml3 <danimoral1001@gmail.com>
2020-09-18 01:57:37 +0200

    davinci: build lib(hwbinder)(hidltransport)
    Signed-off-by: daniml3 <danimoral1001@gmail.com>

--- device.mk
+++ device.mk
@@ -182,7 +182,9 @@ PRODUCT_PACKAGES += \
     android.hidl.base@1.0 \
     android.hidl.base@1.0_system \
     android.hidl.manager@1.0 \
-    android.hidl.manager@1.0_system
+    android.hidl.manager@1.0_system \
+	libhidltransport \
+	libhidltransport.vendor
 
 # Init scripts
 PRODUCT_COPY_FILES += \
@@ -434,3 +436,8 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_BOOT_JARS += \
     WfdCommon
+
+# Binder
+ PRODUCT_PACKAGES += \
+    libhwbinder \
+    libhwbinder.vendor
