c65762e6793a849de432c3ae5840c06009eee17f
Volodymyr Zhdanov <wight554@gmail.com>
2020-09-29 21:47:10 +0300

    davinci: add bt_stack_qti.conf to soong namespaces

--- device.mk
+++ device.mk
@@ -391,7 +391,11 @@ PRODUCT_COPY_FILES += \
 # Soong namespaces
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH) \
-    hardware/xiaomi
+    hardware/xiaomi \
+	hardware/google/pixel \
+	hardware/google/interfaces \
+	vendor/qcom/opensource/commonsys/packages/apps/Bluetooth \
+    vendor/qcom/opensource/commonsys/system/bt/conf
 
 # Telephony
 PRODUCT_PACKAGES += \
