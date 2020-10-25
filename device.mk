60dfe60b9ae8dfacb71ceef33b40068bad7264e5
Arian <arian.kulmer@web.de>
2020-10-25 13:01:13 +0100

    davinci: Build nxp nfc 2.0 service
    Change-Id: I75af5a56973c5024ab8c748b4fb27023de0ba632

--- device.mk
+++ device.mk
@@ -308,15 +308,24 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.ipsec_tunnels.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.ipsec_tunnels.xml
 
 # NFC
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/nxp/opensource/sn100x
 
 PRODUCT_PACKAGES += \
-    com.android.nfc_extras \
-    NfcNci \
-    SecureElement \
-    Tag
+	com.android.nfc_extras \
+	com.gsma.services.nfc \
+	com.nxp.nfc.nq \
+	libnqnfc_nci_jni \
+	nfc_nci.nqx.default.hw \
+	NQNfcNci \
+	nqnfcee_access.xml \
+	nqnfcse_access.xml \
+	Tag \
+	vendor.nxp.hardware.nfc@2.0-service
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/nfc/libnfc-nci.conf:$(TARGET_COPY_OUT_PRODUCT)/etc/libnfc-nci.conf \
+	$(LOCAL_PATH)/configs/nfc/libnfc-nxp_RF.conf:$(TARGET_COPY_OUT_VENDOR)/libnfc-nxp_RF.conf \
     $(LOCAL_PATH)/configs/nfc/libnfc-nxp.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nxp.conf
 
 PRODUCT_COPY_FILES += \
