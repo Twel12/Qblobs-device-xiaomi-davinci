34ebe689111617689ec7692542857b81cb3836af
SVB22 <shef@intech-stv.ru>
2021-02-24 14:19:30 +0300

    Revert "Provide shim for libcameraservice.so"
    This reverts commit c70da9231b09d17bc1fa0664c911d092d90e292e.

--- device.mk
+++ device.mk
@@ -122,9 +122,6 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     libjni_jpegutil
 
-PRODUCT_PACKAGES += \
-	libmedia_jni_shim
-
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
